7d58f4c34191c76106234eed1071b870440e4ba1
Gregor Wolf <gregor.wolf@gmail.com>
2025-05-27 12:15:50 +0000

    install Cloud Foundry CLI

--- .devcontainer/scripts/install-xdg.sh
+++ .devcontainer/scripts/install-xdg.sh
@@ -1,4 +1,9 @@
 # Install xdg-utils to enable opening a browser from devcontainer
 # Currently no out-of-the box support available see: https://github.com/devcontainers/images/issues/885
+#
+# Add Cloud Foundry CLI repository
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo gpg --dearmor -o /usr/share/keyrings/cli.cloudfoundry.org.gpg
+echo "deb [signed-by=/usr/share/keyrings/cli.cloudfoundry.org.gpg] https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+
 sudo apt-get update
-sudo apt-get install -y xdg-utils
+sudo apt-get install -y xdg-utils cf8-cli
