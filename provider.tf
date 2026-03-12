02670d8ce9cea85f324047d4c01cf4c7cc6b8a39
Gregor Wolf <gregor.wolf@gmail.com>
2026-03-12 07:19:37 +0000

    add SAP/sap-cloud-identity-services provider, upgrade

--- provider.tf
+++ provider.tf
@@ -6,7 +6,11 @@ terraform {
     }
     cloudfoundry = {
       source  = "cloudfoundry/cloudfoundry"
-      version = "1.12.0"
+      version = "1.13.0"
+    }
+    cis = {
+      source  = "SAP/sap-cloud-identity-services"
+      version = "0.4.0-beta1"
     }
   }
 }
@@ -19,4 +23,8 @@ provider "cloudfoundry" {
   api_url  = "https://api.cf.${var.region}${var.extLandscape}.hana.ondemand.com"
   user     = var.cf_user
   password = var.cf_password
+}
+
+provider "cis" {
+  tenant_url = "${var.cistenant_url}"
 }
